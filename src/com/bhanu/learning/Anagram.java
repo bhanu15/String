856bd50365b8efb17fb2f66dadd70e15e0f49d5f
bhanu15 <bhanuprakash.nitk@gmail.com>
2014-11-27 01:53:57 +0530

    anagram

---
+++ src/com/bhanu/learning/Anagram.java
@@ -0,0 +1,44 @@
+package com.bhanu.learning;
+
+import java.util.*;
+
+public class Anagram {
+
+	public static void main(String[] args) {
+		String str = "Hello World";
+		String str1 = "hello worls";
+		char[] chArray = str.toCharArray();
+		char[] chArray1 = str1.toCharArray();
+
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for (char c : chArray) {
+			if (map.containsKey(c)) {
+
+				int size = map.get(c) + 1;
+				map.put(c, size);
+
+			} else {
+				map.put(c, 1);
+			}
+
+		}
+
+		for (char c : chArray1) {
+
+			if (map.containsKey(c)) {
+
+				int size = map.get(c) - 1;
+				if (size == 0) {
+					map.remove(c);
+				} else {
+					map.put(c, size);
+				}
+
+			} else {
+				map.put(c, 1);
+			}
+		}
+
+		System.out.println(map);
+	}
+}
